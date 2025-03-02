5e69314cbc8a0d3ceb78d7a19b84e0c0faef198a
Serengiy <kisaykin20@gmail.com>
2025-03-02 14:44:41 +0700

    app init

---
+++ tests/Feature/API/User/UserCanBeRegisteredTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Feature\API\User;
+
+use Tests\TestCase;
+
+class UserCanBeRegisteredTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
